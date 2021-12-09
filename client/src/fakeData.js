8ad57b512ead394a66e6c8199be823c6da15253d
Alejandro Ramirez <ramalejandro@gmail.com>
2021-12-09 11:04:15 -0600

    feat(Frontend): ✨ Testing FakeData file added

---
+++ client/src/fakeData.js
@@ -0,0 +1,32 @@
+export const fakeData = [
+  {
+    id: "1",
+    name: "Colombia",
+    continent: "Americas",
+    capital: "Bogotá",
+  },
+  {
+    id: "2",
+    name: "México",
+    continent: "Americas",
+    capital: "Ciudad de México",
+  },
+  {
+    id: "3",
+    name: "Perú",
+    continent: "Americas",
+    capital: "Lima",
+  },
+  {
+    id: "4",
+    name: "España",
+    continent: "Europa",
+    capital: "Madrid",
+  },
+  {
+    id: "5",
+    name: "Portugal",
+    continent: "Europa",
+    capital: "Lisboa",
+  },
+];
