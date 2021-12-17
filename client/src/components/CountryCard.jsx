756f33e69838b45e5c6997c65ff9fb1d70407791
Alejandro Ramirez <ramalejandro@gmail.com>
2021-12-17 08:44:24 -0600

    style(Country Card & Search Bar): 💄 Logo & CSS styles added

--- client/src/components/CountryCard.jsx
+++ client/src/components/CountryCard.jsx
@@ -6,7 +6,12 @@ const CountryCard = ({ id, name, continent, flag }) => {
   return (
     <Link
       to={`/country/${id}`}
-      style={{ textDecoration: "none", color: "black" }}
+      style={{
+        textDecoration: "none",
+        color: "#002448",
+        width: "150px",
+        height: "150px",
+      }}
     >
       <div className={styles.card}>
         <img className={styles.cardImg} src={flag} alt={name} />
